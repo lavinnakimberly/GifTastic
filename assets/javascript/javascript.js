d25b8a05e4c644d12f489e3a054ea2de0d862d13
lavinnakimberly <lavinnakimberly@gmail.com>
2017-05-16 20:09:40 -0700

    added API

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,10 +1,26 @@
-	var gifArray = ["cat","bird","dog"]
-	for (var i = 0; i < gifArray.length; i++){
-		var buttons = $("<button>");
-		buttons.text(gifArray[i]);
-		$("#buttons").append(buttons);
+var gifArray = ["cat","bird","dog"]
 
-	}
+for (var i = 0; i < gifArray.length; i++){
+	var buttons = $("<button>");
+	buttons.text(gifArray[i]);
+	buttons.attr("data-animal", gifArray[i]);
+	$("#buttons").append(buttons);
+}
+
+$("button").on("click", function(){
+	var animalType = $(this).attr("data-animal");
+
+	var queryURL = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cats";
+
+     $.ajax({
+       url: queryURL,
+       method: "GET"
+	})
+    .done(function(response){
+    	console.log(response)
+    })
+    
+})
 
 
 
