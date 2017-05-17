297306991bfbe08442cd251f2d2024ff6c5ea822
lavinnakimberly <lavinnakimberly@gmail.com>
2017-05-16 21:27:54 -0700

    pause and animate gifs

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,26 +1,66 @@
-var gifArray = ["cat","bird","dog"]
+var gifArray = ["Cinderella","Little Mermaid","Snow White","Sleeping Beauty","Aladdin"]
 
 for (var i = 0; i < gifArray.length; i++){
 	var buttons = $("<button>");
 	buttons.text(gifArray[i]);
-	buttons.attr("data-animal", gifArray[i]);
+	buttons.attr("data-disney", gifArray[i]);
 	$("#buttons").append(buttons);
 }
 
 $("button").on("click", function(){
-	var animalType = $(this).attr("data-animal");
+	var disneyType = $(this).attr("data-disney");
 
-	var queryURL = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cats";
+	var queryURL = "http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&limit=10&q=" + disneyType;
+
+	$("#giphys").empty();
 
      $.ajax({
        url: queryURL,
        method: "GET"
 	})
+
     .done(function(response){
     	console.log(response)
-    })
     
-})
+    var results = response.data
+
+    for (var i = 0; i < results.length; i++){
+
+    	var imageURL = response.data[i].images.original_still.url;
+
+    	var giphyURL = response.data[i].images.downsized.url;
+
+    	var selectedImage = $("<img>");
+
+    	selectedImage.attr("src", imageURL);
+    	selectedImage.attr("data-pause", imageURL);
+    	selectedImage.attr("data-animate", giphyURL);
+    	selectedImage.attr("data-state", "pause")
+    	selectedImage.attr("alt", "selected image");
+
+    	$("#giphys").append(selectedImage);
+    }
+  })
+});
+
+$("#giphys").on("click", "img", function() {
+    
+    var state = $(this).attr("data-state");
+
+    if (state === "pause"){
+    	var newURL = $(this).attr("data-animate");
+    	$(this).attr("src", newURL);
+    	$(this).attr("data-state", "animate");
+
+       }
+       else{
+       	var newURL = $(this).attr("data-pause");
+       	$(this).attr("src", newURL);
+       	$(this).attr("data-state", "pause"); 
+
+ 		}
+
+ 	})
 
 
 
