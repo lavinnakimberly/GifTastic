99ff8cae8bfbf803b449076fe32a567e00b42641
lavinnakimberly <lavinnakimberly@gmail.com>
2017-05-16 19:49:09 -0700

    created buttons

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,33 @@
+	var gifArray = ["cat","bird","dog"]
+	for (var i = 0; i < gifArray.length; i++){
+		var buttons = $("<button>");
+		buttons.text(gifArray[i]);
+		$("#buttons").append(buttons);
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
